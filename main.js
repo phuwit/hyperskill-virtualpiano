46f7116d54edb8b2dff482534b180357a655d7a3
Phuwit P <phuwitput@gmail.com>
2021-03-14 14:18:26 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,100 @@
+// White Keys
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyA") {
+        console.log('The \'A\' key is pressed.');
+        let audioA = new Audio('audio/A.mp3');
+        audioA.play();
+    }
+
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyS") {
+        console.log('The \'S\' key is pressed.');
+        let audioS = new Audio('audio/S.mp3');
+        audioS.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyD") {
+        console.log('The \'D\' key is pressed.');
+        let audioD = new Audio('audio/D.mp3');
+        audioD.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyF") {
+        console.log('The \'F\' key is pressed.');
+        let audioF = new Audio('audio/F.mp3');
+        audioF.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyG") {
+        console.log('The \'G\' key is pressed.');
+        let audioG = new Audio('audio/G.mp3');
+        audioG.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyH") {
+        console.log('The \'H\' key is pressed.');
+        let audioH = new Audio('audio/H.mp3');
+        audioH.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyJ") {
+        console.log('The \'J\' key is pressed.');
+        let audioJ = new Audio('audio/J.mp3');
+        audioJ.play();
+    }
+});
+
+// Black Keys
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyW") {
+        console.log('The \'W\' key is pressed.');
+        let audioW = new Audio('audio/W.mp3');
+        audioW.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyE") {
+        console.log('The \'E\' key is pressed.');
+        let audioE = new Audio('audio/E.mp3');
+        audioE.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyT") {
+        console.log('The \'T\' key is pressed.');
+        let audioT = new Audio('audio/T.mp3');
+        audioT.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyY") {
+        console.log('The \'Y\' key is pressed.');
+        let audioY = new Audio('audio/Y.mp3');
+        audioY.play();
+    }
+});
+
+document.addEventListener("keypress", function(event) {
+    if (event.code === "KeyU") {
+        console.log('The \'U\' key is pressed.');
+        let audioU = new Audio('audio/U.mp3');
+        audioU.play();
+    }
+});
